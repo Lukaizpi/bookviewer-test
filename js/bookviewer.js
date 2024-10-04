c7caa2797ae2d8b144afe92f26a6bef3d894d1ec
Luken Aizpiolea <157817420+Lukaizpi@users.noreply.github.com>
2024-10-04 09:51:14 +0000

    algohecho

--- js/bookviewer.js
+++ js/bookviewer.js
@@ -21,28 +21,43 @@ export class BookViewer {
         // aurrera, atzera eta bilatu botoiak hasieratu
         // bilatu botoia sakatzean, erabiltzaileak sartu duen isbn-a duen liburua lortu
         // eta handleSearchData funtzioa exekutatu
+        let atz = document.getElementById('atzera')
+        let aur = document.getElementById('aurrera')
+        let bil = document.getElementById('bilatu')
+        bil.onclick = () => {
+            let isbn = document.getElementById('isbn')
+            fetch(this.search_base+isbn).then(r=>r.json()).then(this.handleSearchData)
+        } 
         
     }
 
     extractBookData = (book) => {
         // json objektu egoki bat bueltatu, zure webgunean erabili ahal izateko
+        let liburuOndo= {'isbn': book.isbn[0], 'egilea': book.author_name[0], 'data': book.publish_date[0], 'izenburua': book.title, 'filename': }
         return null;
       };
       
     addBookToData = (book, data) => {
         // data array-ean sartu liburua, eta liburu berriaren posizioa bueltatu
-        return null;
+        data.push(book);
+        return data.length();
     };
 
     handleSearchData = (data) => {
         // lortu liburua data objektutik
         // extractBookData eta addBookToData funtzioak erabili, indizea berria lortuz
         // updateView funtzioa erabili, liburu berria bistaratzeko
+        let liburu = data.docs
+        let liburuOndo = this.extractBookData(liburu)
+        let indizeBerria= this.addBookToData(liburuOndo, this.data)
+
+
     };
 
     updateView() {
         // liburuaren datu guztiak bistaratu
         // liburu kopurua bistaratu
+
     }
 
     nextBook() {
